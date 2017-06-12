b151be1d2ab16832f283bf537894d2a6b7ae827f
Ricardo Andres <rricardo@unicauca.edu.co>
2017-06-11 21:07:01 -0500

    Add model User

---
+++ app/src/main/java/unicauca/movil/tubarberia/models/User.java
@@ -0,0 +1,40 @@
+package unicauca.movil.tubarberia.models;
+
+
+public class User {
+
+    Long idusuario;
+    String nombre, mail, password;
+
+    public Long getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(Long idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
